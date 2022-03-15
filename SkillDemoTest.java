3832d4fb3fb6d0844e6b6304350cc089c1d7557c
charbuurst <chn018@ucsd.edu>
2022-03-14 19:49:49 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(1,2));
+        assertEquals(2, SkillDemo.add(1,1)); // since 1 + 2 is 3, it expected 2, so fails. 
     }
 }
 
